ce4eca7476c4868642bcacc8d82e87dfc724ab98
ddebode <ddebode@gmail.com>
2020-10-26 22:18:32 +0100

    Started setup for multi module project

---
+++ swagger-codegen/src/main/java/com/example/openapi/controller/PetController.java
@@ -0,0 +1,56 @@
+package com.example.openapi.controller;
+
+import io.swagger.api.PetApi;
+import io.swagger.model.ModelApiResponse;
+import io.swagger.model.Pet;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+public class PetController implements PetApi {
+
+    @Override
+    public ResponseEntity<Void> deletePet(Long petId, String apiKey) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> findPetsByStatus(@NotNull @Valid List<String> status) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> findPetsByTags(@NotNull @Valid List<String> tags) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Pet> getPetById(Long petId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> petPost() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePet(@Valid Pet body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> updatePetWithForm(Long petId, String name, String status) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ModelApiResponse> uploadFile(Long petId, String additionalMetadata, @Valid MultipartFile file) {
+        return null;
+    }
+}
