69a3693f546cb5e1fb45dd8a31aa0ed3dc276f02
ddebode <ddebode@gmail.com>
2020-11-15 16:07:37 +0100

    Added a module for spring-fox example

---
+++ spring-fox/src/main/java/com/example/openapi/controller/PetController.java
@@ -0,0 +1,9 @@
+package com.example.openapi.controller;
+
+
+import com.example.openapi.server.endpoint.v3.PetApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetController implements PetApi {
+}
